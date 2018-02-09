03388ed162acf4c23a584c8397590dde1bc387fc
Arash Rasoulzadeh <arashrasoulzadeh@gmail.com>
2018-02-09 22:49:34 +0330

    init

---
+++ run.py
@@ -0,0 +1,68 @@
+# encoding=utf8
+# imports
+import pprint
+
+
+pp = pprint.PrettyPrinter(indent=4)
+# statements
+print("starting application ...")
+
+# data
+path = 'test.txt'
+data = []
+person = []
+time = []
+tweet = []
+
+# read
+with open(path, encoding="utf-8") as f:
+    data += f.readlines()
+
+data = data[1:]
+
+RTcount = 0
+FAVcount = 0
+
+step = 1
+for item in data:
+    if step is 1:
+        time += [item]
+    elif step is 2:
+        person += [item[21:]]
+    elif step is 3:
+        tweet += [item]
+    if step is 4:
+        step = 1
+    else:
+        step += 1
+
+person = list(map(lambda x: x[:len(x)], person))
+
+data = []
+for p, d, t in zip(person, time, tweet):
+    if " RT " in t:
+        RTcount += 1
+    if " @ " in t:
+        FAVcount += 1
+
+    data += [{"person": p, "time": d, "tweet": t}]
+
+unique = []
+[unique.append(item) for item in person if item not in unique]
+
+print("FAV COUNT {},RT COUNT {}, TWEETES {}".format(FAVcount, RTcount, len(tweet)))
+
+# from here on the gui
+ 
+# TODO :: user activitys
+
+# todo :: lda topic extraction  for each month or day  (or by hashtag)
+
+# todo :: rt netowrk for each topic
+
+# TODO :: what is difference of reply and mention
+
+# TODO :: interaction matrix
+
+
+# todo :: rt netowrk
